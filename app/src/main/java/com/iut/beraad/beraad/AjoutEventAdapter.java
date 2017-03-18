609bb9ead39b353456775584a6e1f2f8d597b735
rbretzner <rbretzner@gmail.com>
2017-03-18 16:43:01 +0100

    Ajour "addEvent"
    J’ai réussi la complétion automatique par géolocalisation haha :)

---
+++ app/src/main/java/com/iut/beraad/beraad/AjoutEventAdapter.java
@@ -0,0 +1,34 @@
+package com.iut.beraad.beraad;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.seatgeek.placesautocomplete.PlacesApi;
+import com.seatgeek.placesautocomplete.adapter.AbstractPlacesAutocompleteAdapter;
+import com.seatgeek.placesautocomplete.history.AutocompleteHistoryManager;
+import com.seatgeek.placesautocomplete.model.AutocompleteResultType;
+import com.seatgeek.placesautocomplete.model.Place;
+
+/**
+ * Created by raphaelbretzner on 18/03/2017.
+ */
+
+public class AjoutEventAdapter extends AbstractPlacesAutocompleteAdapter {
+
+    public AjoutEventAdapter(final Context context, final PlacesApi api, final AutocompleteResultType resultType, final AutocompleteHistoryManager history) {
+        super(context, api, resultType, history);
+    }
+
+    @Override
+    protected View newView(final ViewGroup parent) {
+        return LayoutInflater.from(parent.getContext()).inflate(R.layout.autocomplete_item_event, parent, false);
+    }
+
+    @Override
+    protected void bindView(final View view, final Place item) {
+        ((TextView) view).setText(item.description);
+    }
+}
