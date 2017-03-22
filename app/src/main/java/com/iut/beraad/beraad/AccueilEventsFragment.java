e7117d4314b9b49cfb8bd62ac373cfe01b946249
Adrien Lemaire <ladrien2@gmail.com>
2017-03-22 14:24:33 +0100

    +

--- app/src/main/java/com/iut/beraad/beraad/AccueilEventsFragment.java
+++ app/src/main/java/com/iut/beraad/beraad/AccueilEventsFragment.java
@@ -15,7 +15,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import java.util.Date;
 import java.util.SortedSet;
