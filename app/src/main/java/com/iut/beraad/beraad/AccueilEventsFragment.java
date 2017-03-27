154aeaa92e3a98b73f1e2a20350217c0b2f8acb4
Adrien Lemaire <ladrien2@gmail.com>
2017-03-27 13:55:23 +0200

    Récupération des données de la bdd au lancement

--- app/src/main/java/com/iut/beraad/beraad/AccueilEventsFragment.java
+++ app/src/main/java/com/iut/beraad/beraad/AccueilEventsFragment.java
@@ -1,6 +1,7 @@
 package com.iut.beraad.beraad;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
@@ -38,12 +39,12 @@ public class AccueilEventsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         view = inflater.inflate(R.layout.content_accueil_events,container,false);
         this.evenements_trie_date = new TreeSet(new ComparateurParDate());
+        new MyDownloadTask().execute();
 
-        ajouterEvenements();
-        //getPersonnesFromJSON();
+        System.out.println("test test aahahahhahah");
+        System.out.println(evenements_trie_date);
 
         evenementAdapter = new EvenementAdapter(evenements_trie_date);
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView_events);
@@ -85,9 +86,7 @@ public class AccueilEventsFragment extends Fragment {
                             String result = InputStreamOperations.InputStreamToString(inputStream);
                             // On récupère le tableau d'objets qui nous concerne
                             JSONArray array = new JSONArray(result);
-                            System.out.println("suppression en approche");
                             evenements_trie_date.clear();
-                            System.out.println("suppression faite");
                             for (int i=0; i<array.length(); i++) {
                                 System.out.println("ok"+i);
                                 // On récupère un objet JSON du tableau
@@ -122,9 +121,6 @@ public class AccueilEventsFragment extends Fragment {
             }
 
         });
-
-
-
         System.out.println(evenements_trie_date);
         return view;
     }
@@ -170,37 +166,48 @@ public class AccueilEventsFragment extends Fragment {
         }
     }
 
-    public void ajouterEvenements() {
+    class MyDownloadTask extends AsyncTask<Void,Void,Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String myurl= "http://pageperso.iut.univ-paris8.fr/~alemaire/Beraad/index.php?module=evenement&action=resultat_allEvents";
+                URL url = new URL(myurl);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                String result = InputStreamOperations.InputStreamToString(inputStream);
+                // On récupère le tableau d'objets qui nous concerne
+                JSONArray array = new JSONArray(result);
+                evenements_trie_date.clear();
+                for (int i=0; i<array.length(); i++) {
+                    System.out.println("ok"+i);
+                    // On récupère un objet JSON du tableau
+                    JSONObject obj = new JSONObject(array.getString(i));
+                    // On fait le lien Personne - Objet JSON
+                    evenements_trie_date.add(new Evenement(
+                            obj.getString("nomEvent"),
+                            obj.getString("image"),
+                            Integer.parseInt(obj.getString("nbParticipants")),
+                            Integer.parseInt(obj.getString("capacite")),
+                            DateTest.makeDateFromString(obj.getString("dateEvent")),
+                            obj.getString("description"),
+                            new Adresse(obj.getString("numero"),obj.getString("rue"),obj.getString("ville"),obj.getString("codePostal")),
+                            new Personne("Adrien","Lemaire","adrien@gmail.com","https://pbs.twimg.com/profile_images/627117609444581380/7YG7kxA4.png"),
+                            false));
+                    System.out.println("HELLO HELLO HELLO HELLO HELLO HELLO ");
+                }
+            } catch (Exception e) {
+                Log.d("!!!!!!!!!!!!!!!!!","Exception lancé");
+                e.printStackTrace();
+            }
+            return null;
+        }
 
-        evenements_trie_date.add(new Evenement("Tournois de foot", "http://img4.hostingpics.net/pics/6852211195325unscandaledevasionfiscaleeclabousselefootballitalienwebtete021648790152.jpg", 31, 37, new DateTest(2017, 11, 12).getDate(),"A l'occasion du 7829eme anniversaire de XXXX, un tournoi de foot a été organisé pour tous les amoureux de foot... Venez nombreux, plein de cadeaux à gagner !!",
-                new Adresse("17", "allée jean jacques rousseau", "Les Pavillons Sous Bois", "93320"),
-                new Personne("Raphaël", "Bretzner", "rbretzner@gmail.com", ""), false)
-        );
-        evenements_trie_date.add(new Evenement("GROSSE SOIREE CHEZ RAPHAEL", "http://www.csgo.ca/wp-content/uploads/2016/09/party.jpg", 31, 37, new DateTest(2017, 11, 12).getDate(),"Ramener les bières et des gateaux, je m'occupe du reste ! :p",
-                new Adresse("140", "rue de la nouvelle france", "Montreuil", "93100"),
-                new Personne("Adrien", "Lemaire", "alemaire@gmail.com", ""), false)
-        );
-        evenements_trie_date.add(new Evenement("Nouvel an 2046", "http://www.iedrs.com/wp-content/uploads/2017/02/4-paris-moyan-brenn-2.jpg", 31, 37, new DateTest(2017, 03, 28).getDate(),"\"Fetons le nouvel an ensemble !!!!! ehehehehhe",
-                new Adresse("489", "avenue Salvador Allende", "Ivry-Sur-Seine", "92500"),
-                new Personne("Raphaël", "Bretzner", "rbretzner@gmail.com", ""), false)
-        );
-        evenements_trie_date.add(new Evenement("Madame invite : Rezz, Loge21, Moonbase au Trabendo le 7 avril", "http://www.iedrs.com/wp-content/uploads/2017/02/4-paris-moyan-brenn-2.jpg", 31, 37, new DateTest(2017, 11, 12).getDate(),"Soirée au Trabendo, organisé par XXXX. Line up : REZZ , Loge21 & guests",
-                new Adresse("56", "avenue de la forêt", "Paris", "75005"),
-                new Personne("Raphaël", "Bretzner", "rbretzner@gmail.com", ""), false)
-        );
-        evenements_trie_date.add(new Evenement("GROSSE SOIREE CHEZ RAPHAEL", "http://www.csgo.ca/wp-content/uploads/2016/09/party.jpg", 31, 37, new DateTest(2017, 06, 12).getDate(),"Ramener les bières et des gateaux, je m'occupe du reste ! :p",
-                new Adresse("140", "rue de la nouvelle france", "Montreuil", "93100"),
-                new Personne("Adrien", "Lemaire", "alemaire@gmail.com", ""), false)
-        );
-        evenements_trie_date.add(new Evenement("Nouvel an 2046", "http://www.iedrs.com/wp-content/uploads/2017/02/4-paris-moyan-brenn-2.jpg", 31, 37, new DateTest(2017, 07, 12).getDate(),"\"Fetons le nouvel an ensemble !!!!! ehehehehhe",
-                new Adresse("489", "avenue Salvador Allende", "Ivry-Sur-Seine", "92500"),
-                new Personne("Raphaël", "Bretzner", "rbretzner@gmail.com", ""), false)
-        );
+        @Override
+        protected void onPostExecute(Void result) {
+            recyclerView.setAdapter(new EvenementAdapter(evenements_trie_date));
+        }
 
-    }
-
-    public void ajouterEvenement(Evenement e) {
-        evenements_trie_date.add(e);
     }
 
     public int moisToInt(String s) {
@@ -249,43 +256,37 @@ public class AccueilEventsFragment extends Fragment {
         return mois;
     }
 
+    public void ajouterEvenements() {
 
-    public void getPersonnesFromJSON() {
-        try {
-            String myurl= "http://adrien.pre-prod.space/Beraad/index.php?module=evenement&action=resultat_allEvents";
-            URL url = new URL(myurl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.connect();
-//            InputStream inputStream = connection.getInputStream();
-//            String result = InputStreamOperations.InputStreamToString(inputStream);
-//            // On récupère le JSON complet
-//            JSONObject jsonObject = new JSONObject(result);
-//            // On récupère le tableau d'objets qui nous concernent
-//            JSONArray array = new JSONArray(jsonObject.getString("evenement"));
-//            Log.d("!!!!!!!!!!!!!!!!!","no");
-//            for (int i=0; i<array.length(); i++) {
-//                Log.d("!!!!!!!!!!!!!!!!!","hey");
-//                // On récupère un objet JSON du tableau
-//                JSONObject obj = new JSONObject(array.getString(i));
-//                // On fait le lien Personne - Objet JSON
-//                this.evenements_trie_date.add(new Evenement(
-//                        obj.getString("nomEvent"),
-//                        obj.getString("image"),
-//                        Integer.parseInt(obj.getString("nbParticipants")),
-//                        Integer.parseInt(obj.getString("capacite")),
-//                        DateTest.makeDateFromString(obj.getString("date")),
-//                        obj.getString("description"),
-//                        new Adresse("140","rue de la Nouvelle France","Montreuil","93100"),
-//                        new Personne("Adrien","Lemaire","adrien@gmail.com","https://pbs.twimg.com/profile_images/627117609444581380/7YG7kxA4.png"),
-//                        false));
-//
-//            }
+        evenements_trie_date.add(new Evenement("Tournois de foot", "http://img4.hostingpics.net/pics/6852211195325unscandaledevasionfiscaleeclabousselefootballitalienwebtete021648790152.jpg", 31, 37, new DateTest(2017, 11, 12).getDate(),"A l'occasion du 7829eme anniversaire de XXXX, un tournoi de foot a été organisé pour tous les amoureux de foot... Venez nombreux, plein de cadeaux à gagner !!",
+                new Adresse("17", "allée jean jacques rousseau", "Les Pavillons Sous Bois", "93320"),
+                new Personne("Raphaël", "Bretzner", "rbretzner@gmail.com", ""), false)
+        );
+        evenements_trie_date.add(new Evenement("GROSSE SOIREE CHEZ RAPHAEL", "http://www.csgo.ca/wp-content/uploads/2016/09/party.jpg", 31, 37, new DateTest(2017, 11, 12).getDate(),"Ramener les bières et des gateaux, je m'occupe du reste ! :p",
+                new Adresse("140", "rue de la nouvelle france", "Montreuil", "93100"),
+                new Personne("Adrien", "Lemaire", "alemaire@gmail.com", ""), false)
+        );
+        evenements_trie_date.add(new Evenement("Nouvel an 2046", "http://www.iedrs.com/wp-content/uploads/2017/02/4-paris-moyan-brenn-2.jpg", 31, 37, new DateTest(2017, 03, 28).getDate(),"\"Fetons le nouvel an ensemble !!!!! ehehehehhe",
+                new Adresse("489", "avenue Salvador Allende", "Ivry-Sur-Seine", "92500"),
+                new Personne("Raphaël", "Bretzner", "rbretzner@gmail.com", ""), false)
+        );
+        evenements_trie_date.add(new Evenement("Madame invite : Rezz, Loge21, Moonbase au Trabendo le 7 avril", "http://www.iedrs.com/wp-content/uploads/2017/02/4-paris-moyan-brenn-2.jpg", 31, 37, new DateTest(2017, 11, 12).getDate(),"Soirée au Trabendo, organisé par XXXX. Line up : REZZ , Loge21 & guests",
+                new Adresse("56", "avenue de la forêt", "Paris", "75005"),
+                new Personne("Raphaël", "Bretzner", "rbretzner@gmail.com", ""), false)
+        );
+        evenements_trie_date.add(new Evenement("GROSSE SOIREE CHEZ RAPHAEL", "http://www.csgo.ca/wp-content/uploads/2016/09/party.jpg", 31, 37, new DateTest(2017, 06, 12).getDate(),"Ramener les bières et des gateaux, je m'occupe du reste ! :p",
+                new Adresse("140", "rue de la nouvelle france", "Montreuil", "93100"),
+                new Personne("Adrien", "Lemaire", "alemaire@gmail.com", ""), false)
+        );
+        evenements_trie_date.add(new Evenement("Nouvel an 2046", "http://www.iedrs.com/wp-content/uploads/2017/02/4-paris-moyan-brenn-2.jpg", 31, 37, new DateTest(2017, 07, 12).getDate(),"\"Fetons le nouvel an ensemble !!!!! ehehehehhe",
+                new Adresse("489", "avenue Salvador Allende", "Ivry-Sur-Seine", "92500"),
+                new Personne("Raphaël", "Bretzner", "rbretzner@gmail.com", ""), false)
+        );
 
-        } catch (Exception e) {
-            Log.d("!!!!!!!!!!!!!!!!!","Exception lancé");
-            e.printStackTrace();
-        }
+    }
 
+    public void ajouterEvenement(Evenement e) {
+        evenements_trie_date.add(e);
     }
 
 }
