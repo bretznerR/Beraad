1cdee77ee30c9a1a3bfbca883f31e77c4d5892d1
rbretzner <rbretzner@gmail.com>
2017-03-28 14:21:50 +0200

    Add classes

---
+++ app/src/main/java/com/iut/beraad/beraad/MyApplication.java
@@ -0,0 +1,40 @@
+package com.iut.beraad.beraad;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+import com.facebook.FacebookSdk;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by raphaelbretzner on 28/03/2017.
+ */
+
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.raphaelbretzner.facebookconnect",  // replace with your unique package name
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                System.out.println("KeyHash:" + Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            System.out.println("ERRRRRROR");
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+}
