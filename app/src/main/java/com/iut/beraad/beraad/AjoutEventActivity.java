6b4d0d6d2f62374bf7cfe548008254f27e7bd99c
rbretzner <rbretzner@gmail.com>
2017-03-30 19:46:31 +0200

    Ajout dans la base de données, en cours d'enregistrement dans le projet l'image du projet

--- app/src/main/java/com/iut/beraad/beraad/AjoutEventActivity.java
+++ app/src/main/java/com/iut/beraad/beraad/AjoutEventActivity.java
@@ -2,15 +2,23 @@ package com.iut.beraad.beraad;
 
 import android.app.DialogFragment;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.Profile;
 import com.seatgeek.placesautocomplete.DetailsCallback;
@@ -58,6 +66,10 @@ public class AjoutEventActivity extends AppCompatActivity {
     @InjectView(R.id.creerEvent)
     Button creerEvent;
 
+    ImageButton imageButton;
+
+    ImageView imageView;
+
     private Toolbar toolbar;
 
     private String[] adresse;
@@ -65,11 +77,28 @@ public class AjoutEventActivity extends AppCompatActivity {
     String urlParameters;
     DialogFragment newFragmentDate;
     DialogFragment newFragmentHeure;
+    private static int RESULT_LOAD_IMG = 1;
+    String imgDecodableString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_event);
+        imageButton = (ImageButton) findViewById(R.id.buttonLoadPicture);
+        imageView = (ImageView) findViewById(R.id.imgView);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create intent to Open Image applications like Gallery, Google Photos
+                System.out.println("looooooool");
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                System.out.println("looooooool 2");
+                // Start the Intent
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+            }
+        });
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -151,6 +180,47 @@ public class AjoutEventActivity extends AppCompatActivity {
     public void showTimePickerDialog(View v) {
         newFragmentHeure = new TimePickerFragment();
         newFragmentHeure.show(getFragmentManager(), "timePicker");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Image is picked
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
+
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                imgDecodableString = cursor.getString(columnIndex);
+                cursor.close();
+
+                imageButton.setVisibility(View.GONE);
+                imageView.setVisibility(View.VISIBLE);
+                // Set the Image in ImageView after decoding the String
+                imageView.setImageBitmap(BitmapFactory
+                        .decodeFile(imgDecodableString));
+
+
+
+            } else {
+                Toast.makeText(this, "You haven't picked Image",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+                    .show();
+        }
+
     }
 
     class MyDownloadTask extends AsyncTask<Void,Void,Void> {
